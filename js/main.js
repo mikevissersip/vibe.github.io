9276d39768d9195a064d99c69d916301db30c894
mikevissersip <mikevissersip@gmail.com>
2025-07-08 13:04:52 +0200

    verbeteringen

--- js/main.js
+++ js/main.js
@@ -80,26 +80,24 @@
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
+			loop: true,
+			autoplay: true,
+			margin: 0,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			nav: false,
+			autoplayHoverPause: false,
+			items: 1,
+			mouseDrag: false,      // 👈 added
+			touchDrag: false,      // 👈 added
+			pullDrag: false,       // 👈 added
+			freeDrag: false,       // 👈 added
+			navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+			responsive: {
+				0: { items: 1 },
+				600: { items: 1 },
+				1000: { items: 1 }
+			}
 		});
 		$('.carousel-testimony').owlCarousel({
 			center: true,
